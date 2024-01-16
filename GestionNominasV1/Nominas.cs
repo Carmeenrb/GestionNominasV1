99acfdedea95f24203204e8103f440293e19b6e2
Carmeenrb <carmeenrbu@gmail.com>
2024-01-16 09:32:35 +0100

    Creación de la Propiedad NombreEmpleado

--- GestionNominasV1/Nominas.cs
+++ GestionNominasV1/Nominas.cs
@@ -14,5 +14,31 @@ namespace GestionNominasV1
         private string _puesto;
         private int _horasTrabajadas;
         private float _salarioHora;
+
+        //PROPIEDADES
+        /// <summary>
+        /// Nombre del empleado
+        /// </summary>
+        public string NombreEmpleado
+        {
+            get
+            {
+                //Control de contenido
+                if (string.IsNullOrEmpty(_nombre)) throw new Exception("Nombre del trabajador no establecido");
+
+                //Devolucion del valor 
+                return _nombre;
+            }
+            set 
+            {
+                //Validaciion de los datos a establecer 
+                if (string.IsNullOrEmpty(value)) throw new Exception("El nombre del trabajador no puede ser null o cadena vacia");
+
+                //Establecimiento del valor al miembro privado 
+                _nombre = value;
+            }
+        }
+
+
     }
 }
